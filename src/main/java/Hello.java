5c02cf81e2e3d58c2e1652c1d42c2195a7f7bdbc
xiang <1617688968@qq.com>
2020-09-30 20:48:54 +0800

    first commit by xiang

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/*
+ *@Description
+ *@Parameters
+ *@author 1617688968@qq.com
+ *@create 2020-09-30 20:43
+ */public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
